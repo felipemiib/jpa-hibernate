d0abf32485e4981ac0b5f6037485af3abfbb1ec9
felipemiib <felipemiib@gmail.com>
2021-11-22 18:24:23 -0300

    Buscar pessoa pelo ID

--- aulaJPAmaven/src/main/java/application/Programa.java
+++ aulaJPAmaven/src/main/java/application/Programa.java
@@ -9,23 +9,34 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Felipe Figueiredo", "felipe@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Valdenise Barreto", "val@gmail.com");
+		/*
+		Pessoa p1 = new Pessoa(null, "Alexandre Fragoso", "alexandre@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Guilherme Giba", "giba@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Fernanda Guerreira", "fernanda@gmail.com");
+		*/
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); //cria Entity Manager Factory
 		EntityManager em = emf.createEntityManager(); // cria Entity Manager
 		
+		//Criar tabela e inserir dados na tabela criada
+		/* 
 		em.getTransaction().begin(); // para inicar a transação com o banco de dados
 		em.persist(p1); // inserir no banco de dados "PERSIST"
 		em.persist(p2);
 		em.persist(p3);
 		em.getTransaction().commit(); // para finalizar a transação com o COMIT
+		*/
+		
+		
+		//Buscar pessoa pelo ID		
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
 		
 		System.out.println("Inclusão concluída !!!!");
+		em.close();
+		emf.close();
 		
-
 	}
 
 }
